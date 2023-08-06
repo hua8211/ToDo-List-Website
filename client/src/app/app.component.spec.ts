c3a80fb68488a61b02c0743d67598c5b979ebfc7
Hua Jiang <huahua@email.unc.edu>
2023-08-05 21:46:42 -0400

    create front end

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,29 +1,121 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { AppComponent, ITodoItem } from './app.component';
+import { HttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [AppComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
-  });
+  it('should load todo items on ngOnInit', fakeAsync(() => {
+    const mockTodoItems: ITodoItem[] = [
+      { id: '1', title: 'Todo 1', complete: false },
+      { id: '2', title: 'Todo 2', complete: true }
+    ];
+    component.ngOnInit();
+    const req = httpTestingController.expectOne('http://localhost:8080/api/todos');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockTodoItems);
+    tick();
+    expect(component.todoItems).toEqual(mockTodoItems);
+  }));
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('client app is running!');
-  });
+  it('should add a todo item', fakeAsync(() => {
+    const mockTodoItem: ITodoItem = { id: '1', title: 'New Todo', complete: false };
+    component.id = mockTodoItem.id;
+    component.title = mockTodoItem.title;
+    component.addTodo();
+    const req = httpTestingController.expectOne('http://localhost:8080/api/todos');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual({ id: mockTodoItem.id, title: mockTodoItem.title, complete: false });
+    req.flush({});
+    tick();
+    expect(component.id).toBe('');
+    expect(component.title).toBe('');
+    expect(component.complete).toBe(false);
+    const loadTodoItemsReq = httpTestingController.expectOne('http://localhost:8080/api/todos');
+    expect(loadTodoItemsReq.request.method).toBe('GET');
+    loadTodoItemsReq.flush([mockTodoItem]);
+    tick();
+    expect(component.todoItems).toEqual([mockTodoItem]);
+  }));
+
+  it('should not add todo item if title is empty', fakeAsync(() => {
+    spyOn(httpClient, 'post')
+    component.title = '';
+    component.addTodo();
+    expect(httpClient.post).not.toHaveBeenCalled();
+    httpTestingController.expectNone('http://localhost:8080/api/todos');
+    expect(component.todoItems).toEqual([]);
+  }));
+
+  it('should update todo item status', fakeAsync(() => {
+    const mockTodoItem: ITodoItem = { id: '1', title: 'Todo', complete: false };
+    component.todoItems = [mockTodoItem];
+    component.updateTodoStatus(mockTodoItem.id);
+    const req = httpTestingController.expectOne(`http://localhost:8080/api/todos/${mockTodoItem.id}`);
+    expect(req.request.method).toBe('PUT');
+    expect(req.request.body).toEqual({ complete: true });
+    req.flush({});
+    tick();
+    const loadTodoItemsReq = httpTestingController.expectOne('http://localhost:8080/api/todos');
+    expect(loadTodoItemsReq.request.method).toBe('GET');
+    loadTodoItemsReq.flush([mockTodoItem]);
+    tick();
+    expect(component.todoItems[0].complete).toBe(false);
+  }));
+
+  it('should not update todo item if ID is not found', fakeAsync(() => {
+    spyOn(httpClient, 'put')
+    component.todoItems = [
+      { id: '1', title: 'Task 1', complete: false },
+      { id: '2', title: 'Task 2', complete: false }
+    ];
+    const nonExistingId = '3'; 
+    component.updateTodoStatus(nonExistingId);
+    expect(httpClient.put).not.toHaveBeenCalled();
+    httpTestingController.expectNone(`http://localhost:8080  /api/todos/${nonExistingId}`);
+    expect(component.todoItems).toEqual([
+      { id: '1', title: 'Task 1', complete: false },
+      { id: '2', title: 'Task 2', complete: false }
+    ]);
+  }));
+  
+  it('should delete a todo item', fakeAsync(() => {
+    const mockTodoItem: ITodoItem = { id: '1', title: 'Todo', complete: false };
+    component.todoItems = [mockTodoItem];
+    component.deleteTodo(mockTodoItem.id);
+    const req = httpTestingController.expectOne(`http://localhost:8080/api/todos/${mockTodoItem.id}`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush({});
+    tick();
+    const loadTodoItemsReq = httpTestingController.expectOne('http://localhost:8080/api/todos');
+    expect(loadTodoItemsReq.request.method).toBe('GET');
+    loadTodoItemsReq.flush([]);
+    tick();
+    expect(component.todoItems).toEqual([]);
+    expect(component.deleteTodoId).toBe('');
+  }));
 });
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+export interface ITodoItem {
+  id: string
+  title: string
+  complete: boolean
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+
+export class AppComponent implements OnInit{
+  public id = ''
+  public title = ''
+  public complete = false
+  public deleteTodoId = ''
+  public todoItems: ITodoItem[] = []
+
+  constructor(private httpClient: HttpClient) {}
+  
+  async ngOnInit() {
+    this.loadTodoItems()
+  }
+
+  async loadTodoItems() {
+    this.todoItems = (await this.httpClient.get<ITodoItem[]>('http://localhost:8080/api/todos').toPromise()) ?? []
+  }
+
+  async addTodo() {
+    if (this.title.trim() !== '') {
+      await this.httpClient.post('http://localhost:8080/api/todos', {
+        id: this.id,
+        title: this.title,
+        complete: false
+      }).toPromise()
+
+      this.id = ''
+      this.title = ''
+      this.complete = false
+  
+      this.loadTodoItems()
+    }
+  }
+
+  async updateTodoStatus(id: string) {
+    const todoItem = this.todoItems.find(item => item.id === id);
+    if (todoItem) {
+      const updatedStatus = !todoItem.complete;
+      await this.httpClient.put(`http://localhost:8080/api/todos/${id}`, { complete: updatedStatus }).toPromise();
+      this.loadTodoItems()
+    }
+  }
+
+  async deleteTodo(id: string) { 
+    this.deleteTodoId = id
+    await this.httpClient.delete(`http://localhost:8080/api/todos/${this.deleteTodoId}`, {}).toPromise();
+    this.loadTodoItems()
+    this.deleteTodoId = ''
+   }
 }
